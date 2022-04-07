c804c6868bb4f3351a536ee0790ea262fa40db99
Job Doesburg <job.doesburg@gmail.com>
2022-04-07 12:03:56 +0200

    Lint

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import argparse
 import csv
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='The most awesome Selenium Capita Selecta crawler')
-    parser.add_argument('-m', help='mobile or desktop')
-    parser.add_argument('-u', help='single URL to crawl, this takes precedent over the -i option')
-    parser.add_argument('-i', help='path to CSV with domains to crawl')
+    parser = argparse.ArgumentParser(
+        description="The most awesome Selenium Capita Selecta crawler"
+    )
+    parser.add_argument("-m", help="mobile or desktop")
+    parser.add_argument(
+        "-u", help="single URL to crawl, this takes precedent over the -i option"
+    )
+    parser.add_argument("-i", help="path to CSV with domains to crawl")
 
     args = parser.parse_args()
 
     if args.m:
-        assert args.m == 'mobile' or args.m == 'desktop'
+        assert args.m == "mobile" or args.m == "desktop"
 
     return args
+
 
 def crawl_url(url):
     driver = webdriver.Chrome()
@@ -26,8 +31,10 @@ def crawl_url(url):
     elem.clear()
     driver.close()
 
+
 def crawl_list(urls):
     print(urls)
+
 
 def main():
     args = parse_args()
@@ -36,10 +43,11 @@ def main():
         crawl_url(args.u)
     elif args.i:
         assert exists(args.i)
-        with open(args.i, 'r', newline='') as urls_csv:
+        with open(args.i, "r", newline="") as urls_csv:
             reader = csv.reader(urls_csv)
-            urls_with_ranks = list(reader)[1:] # Skip header
+            urls_with_ranks = list(reader)[1:]  # Skip header
             crawl_list(urls_with_ranks)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
