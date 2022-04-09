d37dc9887a68fb2357596070658a17adf79a1772
Job Doesburg <job.doesburg@gmail.com>
2022-04-09 19:06:57 +0200

    Refactor code

---
+++ crawler_src/crawler.py
@@ -0,0 +1,124 @@
+from os import path
+
+from tld import get_fld
+import logging
+import time
+import json
+
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class Crawler:
+    def __init__(self, headless=True, mobile=False, output_dir=""):
+        """
+        Initializes a crawler
+        :param headless:
+        :param mobile:
+        :param output_dir:
+        """
+        self.headless = headless
+        self.mobile = mobile
+        self.output_dir = output_dir
+
+        self.current_url = None
+
+        chrome_options = Options()
+        if self.headless:
+            chrome_options.add_argument("--headless")
+        if self.mobile:
+            mobile_emulation = {"deviceName": "Nexus 5"}
+            chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    @property
+    def crawl_mode(self):
+        return "mobile" if self.mobile else "desktop"
+
+    @property
+    def current_domain(self):
+        return get_fld(self.current_url)
+
+    @property
+    def output_file_prefix(self):
+        return f"{self.current_domain}_{self.crawl_mode}"
+
+    def get_requests(self):
+        """
+        Get the HTTP requests and responses including URL, time and headers.
+        :return: All HTTP requests that were created.
+        """
+        requests = []
+        for request in self.driver.requests:
+            request_data = {
+                "request_url": request.url,
+                "time": request.date.timestamp(),
+                "request_headers": dict(request.headers),
+                "response_headers": dict(request.response.headers),
+            }
+            requests.append(request_data)
+        return requests
+
+    def create_screenshot(self, post_consent=False):
+        """
+        Create a screenshot and save it.
+        :param post_consent: Pre or post accepting cookies
+        """
+        filename = (
+            f"{self.output_file_prefix}_{'post' if post_consent else 'pre'}_consent.png"
+        )
+        filename = path.join(self.output_dir, filename)
+
+        self.driver.save_screenshot(filename)
+
+    def create_json(self, output):
+        """
+        Create a json file containing crawler output
+        :param output: output data
+        """
+        filename = path.join(self.output_dir, f"{self.output_file_prefix}.json")
+        with open(filename, "w") as outfile:
+            json.dump(output, outfile, indent=4)
+
+    def crawl_url(self, url):
+        """
+        Crawls a single url
+        :param url: The url to crawl
+        """
+        self.current_url = url
+
+        logging.info(f'Crawl start: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
+        self.driver.get(url)
+
+        self.create_screenshot()
+
+        requests = self.get_requests()
+        cookies = self.driver.get_cookies()
+
+        # TODO: accept cookies
+
+        self.create_screenshot(post_consent=True)
+
+        self.driver.close()
+        logging.info(f'Crawl end: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
+
+        output = {
+            "website_domain": self.current_domain,
+            "crawl_mode": self.crawl_mode,
+            "post_pageload_url": None,
+            "pageload_start_ts": None,
+            "pageload_end_ts": None,
+            "consent_status": None,
+            "requests": requests,
+            "load_time": None,
+            "cookies": cookies,
+        }
+        self.create_json(output)
+
+    def crawl_urls(self, urls):
+        """
+        Crawls a list of urls.
+        :param urls: The urls to crawl.
+        """
+        for url in urls:
+            self.crawl_url(url)
--- crawler_src/main.py
+++ crawler_src/main.py
@@ -1,24 +1,23 @@
-from os import path
+import os
+from os import path, makedirs
 import argparse
 import csv
-from tld import get_fld
 import logging
-import time
-import json
 
-from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
+from crawler_src.crawler import Crawler
 
-DATA_PATH = path.join(path.dirname(path.abspath(__file__)), '..', 'crawl_data')
+DATA_PATH = path.join(path.dirname(path.abspath(__file__)), "..", "crawl_data")
+if not path.exists(DATA_PATH):
+    makedirs(DATA_PATH)
+
 LOGNAME = "crawl.log"
 logging.basicConfig(filename=path.join(DATA_PATH, LOGNAME), level=logging.INFO)
 
 
 def parse_args():
     """
-    Parses the command line arguments and validates the input
-
-    Returns: arguments
+    Parses the command line arguments and validates the input.
+    :return: arguments
     """
 
     parser = argparse.ArgumentParser(
@@ -42,121 +41,24 @@ def parse_args():
     return args
 
 
-def create_json(crawler_output, filename):
-    """
-    Create a json file containing crawler output
-
-    Parameters:
-    crawler_output (dict): The contents of the JSON to be created
-    filename (string): The name of the output JSON file
-    """
-    with open('%s.json' % filename, 'w') as outfile:
-        json.dump(crawler_output, outfile, indent=4)
-
-
-def get_requests(driver):
-    """
-    Get the HTTP requests and responses including URL, time and headers
-
-    Parameters:
-    driver (object): Webdriver
-    """
-    requests = []
-    for request in driver.requests:
-        request_data = {"request_url": request.url, "time": request.date.timestamp(),
-                        "request_headers": dict(request.headers),
-                        "response_headers": dict(request.response.headers)}
-        requests.append(request_data)
-    return requests
-
-
-def crawl_url(url, output_dir='', mobile=False, headless=False):
-    """
-    Crawls a single url
-
-    Parameters:
-    url (string): The url to crawl
-    output_dir (string): The output directory for the files
-    mobile (bool): Run with a mobile client
-    headless (bool): To run in headless mode
-    """
-    website_domain = get_fld(url)
-    crawl_mode = 'mobile' if mobile else 'desktop'
-
-    output_filename = f"{output_dir}{website_domain}_{crawl_mode}_"
-    output_filename = path.join(DATA_PATH, output_filename)
-
-    chrome_options = Options()
-    if headless:
-        chrome_options.add_argument("--headless")
-    if mobile:
-        mobile_emulation = {"deviceName": "Nexus 5"}
-        chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    driver = webdriver.Chrome(options=chrome_options)
-    logging.info(f'Crawl start: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
-
-    driver.get(url)
-    requests = get_requests(driver)
-    create_screenshot(driver, output_filename, mobile=mobile)
-    cookies = driver.get_cookies()
-    create_screenshot(driver, output_filename, mobile=mobile, post_consent=True)
-    driver.close()
-
-    logging.info(f'Crawl end: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
-    output = {
-        "website_domain": website_domain,
-        "crawl_mode": crawl_mode,
-        "post_pageload_url": None,
-        "pageload_start_ts": None,
-        "pageload_end_ts": None,
-        "consent_status": None,
-        "requests": requests,
-        "load_time": None,
-        "cookies": cookies,
-    }
-    create_json(output, output_filename)
-
-
-def crawl_list(urls):
-    """
-    Crawls a list of urls
-
-    urls (list[string]): The urls
-    """
-    logging.info(f'Crawl start: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
-    print(urls)
-    logging.info(f'Crawl end: {time.strftime("%d-%b-%Y_%H%M", time.localtime())}')
-
-
-def create_screenshot(driver, output_filename, mobile=False, post_consent=False):
-    """
-    Create a screenshot and save it
-
-    Parameters:
-    driver (object): Webdriver
-    output_filename (string): The base location to save the ss
-    post_consent (bool): Pre or post accepting cookies
-    """
-
-    filename = output_filename + f"{'mobile' if mobile else 'desktop'}_{'post' if post_consent else 'pre'}_consent.png"
-    driver.save_screenshot(filename)
-
-
 def main():
-    """Main function"""
-
     args = parse_args()
     headless = bool(not args.H or (args.H and args.H == "headless"))
     mobile = bool(args.m and args.m == "mobile")
 
+    crawler = Crawler(
+        headless=headless, mobile=mobile, output_dir=os.path.abspath(DATA_PATH)
+    )
+
     if args.u:
-        crawl_url(args.u, headless=headless, mobile=mobile)
+        url = args.u
+        crawler.crawl_url(url)
     elif args.i:
         assert path.exists(args.i)
         with open(args.i, "r", newline="") as urls_csv:
             reader = csv.reader(urls_csv)
             urls_with_ranks = list(reader)[1:]  # Skip header
-            crawl_list(urls_with_ranks)
+            crawler.crawl_urls(urls_with_ranks)
 
 
 if __name__ == "__main__":
